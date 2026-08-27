a900fa13b11ace28a7ef0f2c129bf4dcbe6858dd
areebbaig580 <areebbaig580@gmail.com>
2026-08-27 17:42:50 +0530

    Login route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,26 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home"
 import Register from './pages/Register'
-
 import AboutAlbum from './pages/AboutAlbum'
-
 import AllMusic from './pages/AllMusic'
 import AllAlbums from './pages/AllAlbums'
+import Login from './pages/Login'
+
 
 
 const App = () => {
   return (
     <div className='min-h-screen w-full flex flex-col md:flex-row bg-black text-white'>
-  
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/album/:id" element={<AboutAlbum />} />
-          <Route path="/Musics" element={<AllMusic />} />
-          <Route path="/Albums" element={<AllAlbums />} />
-        </Routes>
-      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/album/:id" element={<AboutAlbum />} />
+        <Route path="/Musics" element={<AllMusic />} />
+        <Route path="/Albums" element={<AllAlbums />} />
+      </Routes>
+
 
     </div>
   )
